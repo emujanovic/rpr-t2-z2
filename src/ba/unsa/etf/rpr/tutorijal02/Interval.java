bb6a73edcb2c2e4d520c7299718ac2750825c34f
Esma <emujanovic2@etf.unsa.ba>
2020-11-05 21:21:37 +0100

    Implementirani konstruktori i metode isNull i isIn

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,48 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    private double pocetnaTacka;
+    private double krajnjaTacka;
+    private boolean pripadaPocetna;
+    private boolean pripadaKrajnja;
+
+    public Interval(double pocetnaTacka, double krajnjaTacka, boolean pripadaPocetna, boolean pripadaKrajnja) throws IllegalArgumentException {
+        if (pocetnaTacka > krajnjaTacka) {
+            throw new IllegalArgumentException("Neispravne pocetna i krajnja tacka");
+        }
+        this.pocetnaTacka = pocetnaTacka;
+        this.krajnjaTacka = krajnjaTacka;
+        this.pripadaPocetna = pripadaPocetna;
+        this.pripadaKrajnja = pripadaKrajnja;
+    }
+
+    public Interval() {
+        this.pocetnaTacka = 0;
+        this.krajnjaTacka = 0;
+        this.pripadaPocetna = false;
+        this.pripadaKrajnja = false;
+    }
+
+    public boolean isNull() {
+        return !(this.pripadaPocetna && this.pripadaKrajnja && this.pocetnaTacka == 0 && this.krajnjaTacka == 0);
+    }
+
+    public boolean isIn(double tacka) {
+        if (this.pripadaPocetna && this.pripadaKrajnja) {
+            if (tacka >= this.pocetnaTacka && tacka <= this.krajnjaTacka) return true;
+        }
+        if (this.pripadaPocetna && !this.pripadaKrajnja) {
+            if (tacka >= this.pocetnaTacka && tacka < this.krajnjaTacka) return true;
+        }
+        if (!this.pripadaPocetna && this.pripadaKrajnja) {
+            if (tacka > this.pocetnaTacka && tacka <= this.krajnjaTacka) return true;
+        }
+        if (!this.pripadaPocetna && !this.pripadaKrajnja) {
+            if (tacka > this.pocetnaTacka && tacka < this.krajnjaTacka) return true;
+        }
+
+        return false;
+    }
 
 
 }
